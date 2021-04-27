cf17c34940af111ebfcc91622a99b31439d9b18a
João Vitor Zeppe <vitorzeppe2@gmail.com>
2021-04-27 11:35:27 -0300

    melhoras de UI e criação featured movie

--- src/App.js
+++ src/App.js
@@ -2,23 +2,43 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
 //https://github.com/toandriottibertoni/react-netflix-clone-ui
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
-
+    const [featuredData, setFeatureData] = useState(null);
     useEffect(() => {
         const loadAll = async () => {
+          //Pegando a Lista Total
             let list = await Tmdb.getHomeList();
             //console.log(list);
             setMovieList(list);
+
+
+          //Pegando o Featured
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+            setFeatureData(chosenInfo);
         }
         loadAll();
     }, []);
 
     return (
         <div className="page">
+
+        <Header />
+
+
+        {featuredData && 
+          <FeatureMovie item={featuredData}/>
+        }
+
+
           <section className="lists">
             {movieList.map((item, key)=>(
               <MovieRow key={key} title={item.title} items={item.items} />
